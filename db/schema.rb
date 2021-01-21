59fc208d55c8995a5966a58df5d0ce75542ee2ee
Taketo0718 <68280188+gozin56@users.noreply.github.com>
2021-01-21 23:53:05 +0900

    schemaファイル修正

--- db/schema.rb
+++ db/schema.rb
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2021_01_19_103328) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
